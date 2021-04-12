aeca509ffd69796e58319d6d960efd51da6ff11e
ioancucu <ioancucu84@gmail.com>
2021-04-12 15:37:11 +0200

    created files

---
+++ assets/scripts/topmenu.js
@@ -0,0 +1,12 @@
+function sellyourcrop_onclick(){
+    location.replace("http://127.0.0.1:5500/sellyourcrop.html")
+}
+
+function map_onclick(){
+    location.replace("http://127.0.0.1:5500/index.html")
+
+}
+function signupStandard_onclick(){
+    location.replace("http://127.0.0.1:5500/sellcropdash.html")
+
+}
