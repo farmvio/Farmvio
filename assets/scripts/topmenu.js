eb185a6c861b96cef3c86cfca847eb44dde9e41d
ioancucu <ioancucu84@gmail.com>
2021-04-15 18:17:59 +0200

    Add new modifications

--- assets/scripts/topmenu.js
+++ assets/scripts/topmenu.js
@@ -1,12 +1,12 @@
 function sellyourcrop_onclick(){
-    location.replace("https://farmvio.azurewebsites.net/sellyourcrop.html")
+    location.replace("http://127.0.0.1:5500/sellyourcrop.html")
 }
 
 function map_onclick(){
-    location.replace("https://farmvio.azurewebsites.net/index.html")
+    location.replace("http://127.0.0.1:5500/index.html")
 
 }
 function signupStandard_onclick(){
-    location.replace("https://farmvio.azurewebsites.net/sellcropdash.html")
+    location.replace("http://127.0.0.1:5500/sellcropdash.html")
 
 }
